0a5e800fcc45d4fb095950a722d03c905db881ea
Mkmoise <141993247+Mkmoise@users.noreply.github.com>
2026-07-19 17:28:36 +0300

    Implement delete button for todo list items
    Added delete button functionality to list items.

--- app.js
+++ app.js
@@ -16,15 +16,36 @@ form.addEventListener("submit", function(event) {
 
     alert("The Add button is working!");
 
-let li = document.createElement("li");
+    let li = document.createElement("li");
 
-li.textContent = input.value;
+li.textContent = input.value + " ";
+
+let deleteButton = document.createElement("button");
+
+deleteButton.textContent = "Delete";
+
+li.appendChild(deleteButton);
 
 todoList.appendChild(li);
+    
     count++;
 
     itemsLeft.textContent = count + " items left";
 
     input.value = "";
+
+});
+
+todoList.addEventListener("click", function(event) {
+
+    if (event.target.tagName == "BUTTON") {
+
+        event.target.parentElement.remove();
+
+        count--;
+
+        itemsLeft.textContent = count + " items left";
+
+    }
 
 });
